eb9882980c8c0cdd24cc2aa25a02972cf7016528
Tiago Marques <thymarques@gmail.com>
2018-12-26 16:37:16 -0800

    testing other port

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds243054.mlab.com:43054/todosdb`;
+var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds243054.mlab.com:7557/todosdb`;
 var local = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.connect(remoteDB || local, { useNewUrlParser: true });
