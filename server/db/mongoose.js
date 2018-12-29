c5b1ecb0309aa223b469b55d6cb3904bf9ed792f
Tiago Marques <thymarques@gmail.com>
2018-12-29 12:55:18 -0800

    mbd

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds243054.mlab.com:43054/todosdb`;
-
-var remoteDB = `mongodb+srv://dbdev:${process.env.DB_PASS}@cluster0-hwqpu.mongodb.net/todosdb?retryWrites=true`;
+var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds151955.mlab.com:51955/heroku_d30sf7p8`;
 var local = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.connect(remoteDB || local, { useNewUrlParser: true });
