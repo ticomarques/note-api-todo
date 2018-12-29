84c3feb73d93a40a549a21f2d9efc0ffb7d77bc9
Tiago Marques <thymarques@gmail.com>
2018-12-29 12:20:09 -0800

    testing another mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds243054.mlab.com:43054/todosdb`;
+
 var remoteDB = `mongodb+srv://dbdev:${process.env.DB_PASS}@cluster0-hwqpu.mongodb.net/todosdb?retryWrites=true`;
 var local = 'mongodb://localhost:27017/TodoApp';
 
