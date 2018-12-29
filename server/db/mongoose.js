0dd9a2e7aff19dfa83b140a73c26b852cc38788c
Tiago Marques <thymarques@gmail.com>
2018-12-29 11:50:07 -0800

    adding atlas mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds243054.mlab.com:7557/todosdb`;
+//var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds243054.mlab.com:43054/todosdb`;
+var remoteDB = `mongodb+srv://dbdev:${process.env.DB_PASS}@cluster0-hwqpu.mongodb.net/todosdb?retryWrites=true`;
 var local = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.connect(remoteDB || local, { useNewUrlParser: true });
