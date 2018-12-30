6a6d4055d38308995ad89e2fde2a886e1a290c4c
Tiago Marques <thymarques@gmail.com>
2018-12-30 10:58:01 -0800

    using default var

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-
-var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds151955.mlab.com:51955/heroku_d30sf7p8`;
-var local = 'mongodb://localhost:27017/TodoApp';
+const local = 'mongodb://localhost:27017/TodoApp';
 
 mongoose.connect(process.env.MONGODB_URI || local, { useNewUrlParser: true });
 
