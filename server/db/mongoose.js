f4ef4f2a7b79f48b2677a40897fc907249fdfc9a
Tiago Marques <thymarques@gmail.com>
2018-12-30 10:49:56 -0800

    adding var

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds243054.mlab.com:43054/todosdb`;
 var remoteDB = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds151955.mlab.com:51955/heroku_d30sf7p8`;
 var local = 'mongodb://localhost:27017/TodoApp';
 
-mongoose.connect(remoteDB || local, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || local, { useNewUrlParser: true });
 
 module.exports = {
     mongoose
